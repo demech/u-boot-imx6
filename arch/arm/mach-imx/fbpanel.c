f5319de5f8978da499d6fbf4f9e1961344152df4
Troy Kisky <troy.kisky@boundarydevices.com>
2021-02-04 11:26:38 -0800

    fbpanel: add lt8912 status setting
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- arch/arm/mach-imx/fbpanel.c
+++ arch/arm/mach-imx/fbpanel.c
@@ -212,6 +212,7 @@ static const char *const aliases[] = {
 [FBTS_ST1633I] = "ts_st1633i",
 [FBTS_TSC2004] = "ts_tsc2004",
 [FBP_MIPI_TO_LVDS] = "mipi_to_lvds",
+[FBP_LT8912] = "lt8912",
 [FBP_SPI_LCD] = "spi_lcd",
 [FBP_PCA9540] = "pca9540",
 [FBP_PCA9546] = "pca9546",
